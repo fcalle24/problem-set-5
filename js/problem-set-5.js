2b92e9047026795094d8f81685002c2941f76e82
Fabian Calle <facalle24@gmail.com>
2018-11-21 10:03:29 -0500

    nothing new

--- js/problem-set-5.js
+++ js/problem-set-5.js
@@ -29,6 +29,10 @@ function mario() {
   var op1 = document.getElementById("output1");
   height = prompt('Please enter a height within the range of of 1-23.');
 
+  while (height <= 23) {
+    height = prompt("Please re-enter height value.");
+  }
+
   ////////////////////////// DO NOT MODIFY
   check('mario', height); // DO NOT MODIFY
   ////////////////////////// DO NOT MODIFY
